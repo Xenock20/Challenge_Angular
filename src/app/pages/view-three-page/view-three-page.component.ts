39da52b09aa2733bf507a761843617941c2861fe
Xenock20 <juliosaboredo@gmail.com>
2023-10-27 17:57:12 -0300

    Finaly challenge

--- src/app/pages/view-three-page/view-three-page.component.ts
+++ src/app/pages/view-three-page/view-three-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-view-three-page',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-three-page.component.css']
 })
 export class ViewThreePageComponent {
+  opciones = ['Opción 1', 'Opción 2', 'Opción 3'];
+  selectedOption: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  formOption1: FormGroup;
+  formOption2: FormGroup;
+  formOption3: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formOption1 = this.fb.group({
+      campo1: ['', Validators.required],
+      campo2: [''],
+    });
+
+    this.formOption2 = this.fb.group({
+      campoA: ['', Validators.required],
+      campoB: [''],
+    });
+
+    this.formOption3 = this.fb.group({
+      campoX: ['', Validators.required],
+      campoY: [''],
+    });
+  }
+
+  setSelectedOption(option: string) {
+    this.selectedOption.next(option);
+  }
+
+  submitForm(form: FormGroup) {
+    if (form.valid) {
+      console.log(form.value);
+    } else {
+      console.error("Invalido");
+      
+    }
+  }
   
 }
